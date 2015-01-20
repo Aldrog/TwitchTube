4ae69fd863ed703b0715eb2210ee1a86092b3082
Aldrog <penkrat8@gmail.com>
2015-01-20 06:32:14 +0300

    Add licensing header.

--- src/harbour-twitchtube.cpp
+++ src/harbour-twitchtube.cpp
@@ -1,10 +1,28 @@
+/*
+ * Copyright © 2015 Andrew Penkrat
+ *
+ * This file is part of TwitchTube.
+ *
+ * TwitchTube is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TwitchTube is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TwitchTube.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifdef QT_QML_DEBUG
 #include <QtQuick>
 #endif
 
 #include <sailfishapp.h>
 #include "ircchat.h"
-
 
 int main(int argc, char *argv[])
 {
